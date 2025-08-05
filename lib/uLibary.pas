a61e2e1b7b2836675f0f0ed1f6725d0c9bbb9f1c
Gustavo Sequeira <sequeira2003@gmail.com>
2025-08-04 22:43:28 -0300

    funcoes

--- lib/uLibary.pas
+++ lib/uLibary.pas
@@ -7,13 +7,17 @@ type
   public
     function MesValido(const AMes: string): Boolean;
     function RetornarPalavra(const Texto: string; Posicao: integer): string;
-    function AnoValido(const Texto: string; out Ano: Integer): Boolean;   
+    function AnoValido(const Texto: string; out Ano: Integer): Boolean;
+    function MelhorarTexto(const TextoOriginal: string): string;
+    function SegundosAleatorios: Integer;
+    function GerarMensagemLembrete(const Nome: string; const Horario: string): string;
   end;
 
 implementation
 
 uses
-  System.SysUtils, System.StrUtils, System.Classes;
+  System.Net.URLClient, System.Net.HttpClient, System.Net.HttpClientComponent,
+  System.SysUtils, System.StrUtils, System.JSON, System.Classes;
 
 function TLibary.MesValido(const AMes: string): Boolean;
 const
@@ -58,6 +62,82 @@ end;
 function TLibary.AnoValido(const Texto: string; out Ano: Integer): Boolean;
 begin
   Result := TryStrToInt(Texto, Ano) and (Ano >= 2025) and (Ano <= 2030);
+end;
+
+function TLibary.MelhorarTexto(const TextoOriginal: string): string;
+const
+  API_KEY = 'SUA_CHAVE_AQUI';
+  URL = 'https://api.openai.com/v1/chat/completions';
+var
+  HttpClient: TNetHTTPClient;
+  ReqBody, MsgObj, RootObj: TJSONObject;
+  Msgs: TJSONArray;
+  Response: IHTTPResponse;
+  RespJSON: TJSONObject;
+begin
+  Result := '';
+  HttpClient := TNetHTTPClient.Create(nil);
+  try
+    HttpClient.CustomHeaders['Authorization'] := 'Bearer ' + API_KEY;
+    HttpClient.ContentType := 'application/json';
+
+    // Montar o corpo da requisição
+    MsgObj := TJSONObject.Create;
+    MsgObj.AddPair('role', 'user');
+    MsgObj.AddPair('content', TextoOriginal);
+
+    Msgs := TJSONArray.Create;
+    Msgs.AddElement(MsgObj);
+
+    ReqBody := TJSONObject.Create;
+    ReqBody.AddPair('model', 'gpt-3.5-turbo');
+    ReqBody.AddPair('messages', Msgs);
+
+    // Enviar requisição
+    Response := HttpClient.Post(URL, TStringStream.Create(ReqBody.ToJSON, TEncoding.UTF8), nil);
+
+    // Tratar resposta
+    RespJSON := TJSONObject.ParseJSONValue(Response.ContentAsString) as TJSONObject;
+    try
+      Result := RespJSON.GetValue<TJSONArray>('choices').Items[0].GetValue<TJSONObject>('message').GetValue<string>('content').Trim;
+    finally
+      RespJSON.Free;
+    end;
+  finally
+    HttpClient.Free;
+  end;
+end;
+
+function TLibary.SegundosAleatorios: Integer;
+begin
+  Randomize; 
+  Result := Random(20) + 1; 
+end;
+
+function TLibary.GerarMensagemLembrete(const Nome: string; const Horario: string): string;
+const
+  Versiculos: array[0..3] of string = (
+    '"Sede firmes e constantes, sempre abundantes na obra do Senhor." (1Co 15:58)',
+    '"Rogai, pois, ao Senhor da seara que envie obreiros para a sua seara." (Mt 9:38)',
+    '"Cada um exerça o dom que recebeu para servir aos outros." (1Pe 4:10)',
+    '"Tudo quanto fizerdes, fazei-o de coração, como ao Senhor." (Cl 3:23)'
+  );
+
+  Mensagens: array[0..3] of string = (
+    'Que alegria tê-lo servindo ao Senhor conosco!',
+    'Que sua dedicação inspire e edifique a todos!',
+    'Deus fortaleça seu coração neste serviço!',
+    'Que sua presença seja canal de bênção nesta obra!'
+  );
+var
+  Versiculo, Saudacao: string;
+begin
+  Randomize;
+  Versiculo := Versiculos[Random(Length(Versiculos))];
+  Saudacao := Mensagens[Random(Length(Mensagens))];
+
+  Result := Format('Olá, irmão/irmã %s! Você está escalado para o culto às %s. %s %s',
+                   [Nome, Horario, Versiculo, Saudacao]);
 end;
 
 end.
